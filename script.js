10ab3b9ae9d5d73e5c6c61e68fece3140cfbb0b5
irtiza <irtiza.zia05@gmail.com>
2026-05-01 17:24:43 +0100

    Create function for computer's choice
    - script.js: Create function getComputerChoice which randomly picks between Rock (Gu), Paper (Chi), or Scissors (Pa)

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("He")
+function getComputerChoice() {
+  let randDec = Math.random();
+  
+  if (randDec >= (1 / 3) && randDec <= (2 / 3))
+    return "Rock - Gu"
+  else if (randDec < (1 / 3))
+    return "Scissors - Chi"
+  else {
+    return "Paper - Pa"
+  }
+}
+
+console.log(getComputerChoice())
