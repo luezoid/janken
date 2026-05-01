05c0bc3936fbf2d596bdb6c1b99ad4d2a520ac2d
irtiza <irtiza.zia05@gmail.com>
2026-05-01 17:50:48 +0100

    Create round system
    - script.js: Create playRound function and write the algorithm to check who wins between the user and the computer

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ let humanScore, computerScore = 0;
 function getComputerChoice() {
   let randDec = Math.random();
   
-  if (randDec >= (1 / 3) && randDec <= (2 / 3))
+  if (randDec >= (1 / 3) && randDec <= (2 / 3)) {
     return "Rock - Gu"
-  else if (randDec < (1 / 3))
+  } else if (randDec < (1 / 3)) {
     return "Scissors - Chi"
-  else {
+  } else {
     return "Paper - Pa"
   }
 }
@@ -19,7 +19,16 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
+  let user = humanChoice.toLowercase();
 
+  if ((user === "rock" && computerChoice === "Rock - Gu") || (user === "paper" && computerChoice === "Paper - Pa") || (user === "scissors" && computerChoice === "Scissors - Chi")) {
+    return "Draw!" 
+  } else if ((user === "rock" && computerChoice === "Scissors - Chi") || (user === "paper" && computerChoice === "Rock - Gu") || (user === "scissors" && computerChoice === "Paper - Pa")) {
+    return "You win!"
+  } else {
+    return "You lose!"
+  }
+    
 }
 
 console.log(getComputerChoice())
