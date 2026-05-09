5938583251ca775f178dd3012459f58ef309d7b7
irtiza <irtiza.zia05@gmail.com>
2026-05-09 18:00:36 +0100

    Add the current score to the end of each round
    - script.js: Add the current score to the end of each paragraph being appended

--- script.js
+++ script.js
@@ -39,13 +39,13 @@ function showResult(str) {
 
 function playRound(humanChoice, computerChoice) {
   if ((humanChoice === "r" && computerChoice === "r") || (humanChoice === "p" && computerChoice === "p") || (humanChoice === "s" && computerChoice === "s")) {
-    showResult(`Draw! You both picked ${getFullName(humanChoice)}! No points!`)
+    showResult(`Draw! You both picked ${getFullName(humanChoice)}! No points! Current Score: ${humanScore}-${computerScore}`)
     return "d"
   } else if ((humanChoice === "r" && computerChoice === "s") || (humanChoice === "p" && computerChoice === "r") || (humanChoice === "s" && computerChoice === "p")) {
-    showResult(`You win one point! ${getFullName(humanChoice)} beats ${getFullName(computerChoice)}!`)
+    showResult(`You win one point! ${getFullName(humanChoice)} beats ${getFullName(computerChoice)}! Current Score: ${humanScore}-${computerScore}`)
     return "h"
   } else {
-    showResult(`Computer wins one point! ${getFullName(computerChoice)} beats ${getFullName(humanChoice)}!`)
+    showResult(`Computer wins one point! ${getFullName(computerChoice)} beats ${getFullName(humanChoice)}! Current Score: ${humanScore}-${computerScore}`)
     return "c"
   }
 }
